4e68c13ee76b31a088fbd0015da8e388662b4d5d
Miura Hideki <m-72@tf6.so-net.ne.jp>
2016-06-12 19:12:01 +0900

    Refact inline flag

--- include/mruby/jitcode.h
+++ include/mruby/jitcode.h
@@ -666,9 +666,6 @@ class MRBJitCode: public MRBGenericCodeGenerator {
     emit_load_literal(mrb, coi, reg_tmp0, 0);
     emit_move(mrb, coi, reg_context, OffsetOf(mrb_callinfo, env), reg_tmp0);
     emit_move(mrb, coi, reg_context, OffsetOf(mrb_callinfo, err), reg_tmp0);
-    if (irep->jit_inlinep == 0 || 1) {
-      //emit_move(mrb, coi, reg_tmp1, OffsetOf(mrb_callinfo, jit_entry), reg_tmp0);
-    }
 
     /* Inherit eidx/ridx */
     emit_move(mrb, coi, reg_tmp0, reg_tmp1, OffsetOf(mrb_callinfo, eidx));
@@ -728,7 +725,7 @@ class MRBJitCode: public MRBGenericCodeGenerator {
       gen_stack_extend(mrb, status, coi, callee_nregs, n + 2);
     }
 
-    if (irep->jit_inlinep == 0 || 1) {
+    if (1 || !irep->jit_inlinep) {
       gen_set_jit_entry(mrb, pc, coi, irep);
     }
 
@@ -1828,16 +1825,24 @@ class MRBJitCode: public MRBGenericCodeGenerator {
       emit_move(mrb, coi, reg_tmp1, reg_context, OffsetOf(mrb_context, ci));
     }
 
-    /* Set return address from callinfo */
-    emit_move(mrb, coi, reg_tmp0, reg_tmp1, OffsetOf(mrb_callinfo, jit_entry));
-    test(reg_tmp0, reg_tmp0);
-    jnz("@f");
+    if (!(*status->irep)->jit_inlinep) {
+      /* Set return address from callinfo */
+      emit_move(mrb, coi, reg_tmp0, reg_tmp1, OffsetOf(mrb_callinfo, jit_entry));
+      test(reg_tmp0, reg_tmp0);
+      jnz("@f");
 
-    L(".reg_vm");
-    gen_exit(mrb, NULL, 1, 1, status, coi);
+      L(".reg_vm");
+      gen_exit(mrb, NULL, 1, 1, status, coi);
 
-    L("@@");
-    emit_jmp(mrb, coi, reg_tmp0);
+      L("@@");
+      emit_jmp(mrb, coi, reg_tmp0);
+    }
+    else {
+      emit_jmp(mrb, coi, "@f");
+      L(".reg_vm");
+      gen_exit(mrb, NULL, 1, 1, status, coi);
+      L("@@");
+    }
 
     outLocalLabel();
 
